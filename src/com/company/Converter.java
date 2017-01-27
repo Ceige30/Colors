4efe16fbf849fc19e576f9aede6637df6e0e943a
BlackMaria <sig91308@gmail.com>
2017-01-26 22:24:23 -0500

    Well well well well

---
+++ src/com/company/Converter.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Converter {
+
+    public static void colorIndex (String color) throws IOException {
+        int [] RGB = new int [3];
+        boolean specific = false;
+
+        File colorsList = new File("src/Colors.txt");
+        Scanner checkColors = new Scanner(colorsList);
+        while (checkColors.hasNext()) {
+            String line = checkColors.nextLine();
+            String [] divisions = line.split("\\s+");
+            divisions[0] = divisions[0].toLowerCase();
+            if (divisions[0].equals(color.toLowerCase())) {
+                specific = true;
+
+                RGB[0] = Integer.parseInt(divisions[1].substring(0, 2), 16);
+                RGB[1] = Integer.parseInt(divisions[1].substring(2, 4), 16);
+                RGB[2] = Integer.parseInt(divisions[1].substring(4, 6), 16);
+
+                System.out.println("HTML Color: " + divisions[0].toUpperCase());
+                System.out.println("Hexadecimal: " + divisions[1]);
+                System.out.println("RGB Values: " + RGB[0] + "," + RGB[1] + "," + RGB[2]);
+                break;
+            }
+        }
+        if (!specific) {
+            System.out.println("Error no valid data found");
+        }
+    }
+
+    public static void convertToHex (int [] RGB) throws IOException {
+        String hex;
+        String [] hexValues = new String [3];
+        hexValues[0] = Integer.toHexString(RGB[0]);
+        hexValues[1] = Integer.toHexString(RGB[1]);
+        hexValues[2] = Integer.toHexString(RGB[2]);
+        hex = hexValues[0] + hexValues[1] + hexValues[2];
+        System.out.println("Hexadecimal: " + hex);
+        System.out.println("RGB Values: " + RGB[0] + "," + RGB[1] + "," + RGB[2]);
+
+        boolean specific = false;
+
+        File colors = new File("src/Colors.txt");
+        Scanner checkColors = new Scanner(colors);
+        while (checkColors.hasNext()) {
+            String line = checkColors.nextLine();
+            String [] divisions = line.split("\\s+");
+            if (divisions[1].equals(hex.toLowerCase())) {
+                specific = true;
+                System.out.println("HTML Color: " + divisions[0]);
+                break;
+            }
+        }
+        if (!specific) {
+            System.out.println("HTML Color: No specific color associated that fits the description");
+        }
+    }
+
+    public static void convertToRGB (String hex) throws IOException {
+        int [] RGB = new int [3];
+        RGB[0] = Integer.parseInt(hex.substring(0, 2), 16);
+        RGB[1] = Integer.parseInt(hex.substring(2, 4), 16);
+        RGB[2] = Integer.parseInt(hex.substring(4, 6), 16);
+
+        System.out.println("Hexadecimal: " + hex);
+        System.out.println("RGB Values: " + RGB[0] + "," + RGB[1] + "," + RGB[2]);
+
+        boolean specific = false;
+
+        File colors = new File("src/Colors.txt");
+        Scanner checkColors = new Scanner(colors);
+        while (checkColors.hasNext()) {
+            String line = checkColors.nextLine();
+            String [] divisions = line.split("\\s+");
+            if (divisions[1].equals(hex.toLowerCase())) {
+                specific = true;
+                System.out.println("COLOR: " + divisions[0]);
+                break;
+            }
+        }
+        if (!specific) {
+            System.out.println("HTML Color: No specific color associated that fits the description");
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,68 @@
 package com.company;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) throws IOException {
         Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        int [] RGB = new int [3];
+        boolean looper = true;
+        boolean looperPrt2 = true;
+        String cPick = "";
         String cAnswer = "";
 
-        boolean looper = true;
-        while(looper)
+        while (looper) {
             System.out.println("What are you putting in to be converted?");
             System.out.println("The options are RBG, HTML or hexadecimal");
-            try{
-                cAnswer = sc1.nextLine().toLowerCase();
-                if(cAnswer.equals("html") || cAnswer.equals("rgb") || cAnswer.equals("hexadecimal"))
-                    looper = false;
-            }catch(){
-                System.out.println("Invalid input");
+
+            cPick = sc1.next().toLowerCase();
+            if (cPick.equals("html")) {
+                looper = false;
+                System.out.println("Enter an HTML color.");
+                cAnswer = sc2.nextLine().toLowerCase();
+                Converter.colorIndex(cAnswer);
+
+            } else if (cPick.equals("rgb")) {
+                looper = false;
+                for (int i = 0; i < RGB.length; i++) {
+                    while (looperPrt2) {
+                        if (i == 0) {
+                            System.out.println("Enter an RGB value for Red (0-255, both numbers inclusive)");
+                        } else if (i == 1) {
+                            System.out.println("Enter an RGB value for Green (0-255, both numbers inclusive)");
+                        } else {
+                            System.out.println("Enter an RGB value for Blue (0-255, both numbers inclusive)");
+                        }
+                        RGB[i] = sc2.nextInt();
+                        if (RGB[i] < 0 || RGB[i] > 255) {
+                            System.out.println("Come on enter something between 0-255");
+                        } else {
+                            looperPrt2 = false;
+                        }
+                    }
+                    looperPrt2 = true;
+                }
+                Converter.convertToHex(RGB);
+
+            } else if (cPick.equals("hexadecimal")) {
+                looper = false;
+                while (looperPrt2) {
+                    System.out.println("Enter a valid hexadecimal");
+                    cAnswer = sc2.nextLine().toLowerCase();
+                    if (cAnswer.length() == 6) {
+                        looperPrt2 = false;
+                    } else {
+                        System.out.println("The hexadecimal inputted is not 6 characters in length.");
+                    }
+                }
+                Converter.convertToRGB(cAnswer);
+
+            } else {
+                System.out.println("Please input either RBG HTML or hexadecimal not "+cPick);
             }
-        Scanner sc2 = new Scanner(System.in);
-        if(cAnswer.equals("html")){
-
-        }else if(cAnswer.equals("rgb")){
-
-        }else if(cAnswer.equals("hexadecimal")){
-
-        }else
-            System.out.println("Ok man how the heck did you get here look just go back and try again");
+        }
     }
 }
